e576e5944abcc445f1d5674def0ff75d6a6936ac
Jsbro98 <Jsbro98@github.com>
2024-04-09 23:45:39 -0500

    Changed DataHandler to interop with Todo. Asjusted access modifiers and added new methods

--- Todo/DataHandler.cs
+++ Todo/DataHandler.cs
@@ -5,19 +5,17 @@ namespace Todo
     public class DataHandler
     {
 
-        private List<string> _data;
+        private readonly List<string> _data;
         private const string _path = "data.csv";
 
-        public DataHandler()
+        public DataHandler(List<string> list)
         {
-            _data = [];
+            _data = list;
 
             if (!File.Exists(_path))
             {
                 File.Create($@"./{_path}").Close();
             }
-
-            ReadCSV();
         }
 
         public void WriteToCSV(string data)
@@ -25,22 +23,84 @@ namespace Todo
             try
             {
 
-                string input = $"{data},";
+                string input;
+
+                if (_data.Count > 0)
+                {
+                    input = $",{data}";
+                } else
+                {
+                    input = $"{data}";
+                }
 
                 using StreamWriter sw = new(_path, true);
 
                 sw.Write(input);
-                _data.Add(input);
+                _data.Add(data);
 
-            } catch (Exception e)
+            } catch (IOException e)
             {
                 Console.WriteLine(e.Message);
+                throw;
             }
         }
 
-        private void ReadCSV()
+        public void Remove(string key)
         {
-            _data = [.. File.ReadAllLines(_path)];
+            try
+            {
+                _data.Remove(key);
+                Reset(false);
+                
+                using StreamWriter sw = new(_path, true);
+                for (int i = 0; i < _data.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        sw.Write(_data[i]);
+                        continue;
+                    }
+
+                    sw.Write($",{_data[i]}");
+                }
+
+            } catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public void Reset(bool clearDataToo)
+        {
+            if (clearDataToo)
+            {
+                _data.Clear();
+            }
+
+            using StreamWriter sw = new(_path);
+            sw.Write("");
+        }
+
+        internal List<string> Data
+        {
+            get { return _data; }
+        }
+
+        internal void ReadCSV()
+        {
+            string[] words = File.ReadAllLines(_path);
+
+            if (!words[0].Contains(',') && words.Length > 0)
+            {
+                _data.Add(words[0]);
+                return;
+            }
+            
+            foreach(string word in words[0].Split(','))
+            {
+                _data.Add(word);
+            }
         }
     }
 }
